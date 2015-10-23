307ca18604e578035634a69fa4421a873e8bbc05
Peter Styk <polfilm@Peters-MacBook.local>
2015-10-23 18:02:30 +0100

    1.9 fixed now working on for sequential positive numbers with step 1

--- 1.9.groovy
+++ 1.9.groovy
@@ -1,3 +1,5 @@
+//assignment 1.9
+
 boolean isSequenceUp = true
 String sNr = ""
 Integer iNr = 0
@@ -8,9 +10,9 @@ while (sNr != "-1"){
     sNr = System.console().readLine()
     iNr = Integer.parseInt(sNr)
     
-	//make sure fail is saved only once, do not count exit as fail
-	if (isSequenceUp && sNr != "-1") {
-	    if (iNr <= iNrPrev){
+	//make sure fail is saved only once, do not count exit as fail, skip check when no previous value
+	if (isSequenceUp && sNr != "-1" && iNrPrev > 0) {
+	    if (iNrPrev+1 != iNr){
 		    isSequenceUp = false
 		}	
 	}
